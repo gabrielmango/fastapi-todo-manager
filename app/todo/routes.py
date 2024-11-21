a70c8448d3eac35171d6943c1032436f51609e6f
Gabriel Mango <gabrielhmango@gmail.com>
2024-11-21 15:22:35 -0300

    feat: create endpoint to update one task

--- app/todo/routes.py
+++ app/todo/routes.py
@@ -1,6 +1,7 @@
 from fastapi import APIRouter, HTTPException
 
-from app.todo.crud import create_todo, get_all_todos, get_todo_by_id
+from app.todo.crud import (create_todo, get_all_todos, get_todo_by_id,
+                           update_todo)
 from app.todo.models import Todo
 
 todo_router = APIRouter()
@@ -23,3 +24,11 @@ def retrieve_todo(todo_id: int):
 def add_todo(todo: Todo):
     create_todo(todo.dict())
     return {'message': 'Todo created successfully'}
+
+
+@todo_router.put('/{todo_id}')
+def modify_todo(todo_id: int, todo: Todo):
+    updated_todo = update_todo(todo_id, todo.dict())
+    if not updated_todo:
+        raise HTTPException(status_code=404, detail='Todo not found')
+    return updated_todo
