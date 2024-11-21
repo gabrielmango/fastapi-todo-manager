aa886b982647aade2eda302b4d9c9e42bf45a1d1
Gabriel Mango <gabrielhmango@gmail.com>
2024-11-21 14:21:59 -0300

    feat: add save_data() for load data from tasks.json

--- app/database.py
+++ app/database.py
@@ -1,11 +1,16 @@
 import json
 from pathlib import Path
 
-DATABASE_FILE = Path(__file__).parent.parent / "tasks.json"
+DATABASE_FILE = Path(__file__).parent.parent / 'tasks.json'
+
 
 def load_data():
     if DATABASE_FILE.exists():
-        with open(DATABASE_FILE, "r") as f:
+        with open(DATABASE_FILE, 'r') as f:
             return json.load(f)
     return []
 
+
+def save_data(data):
+    with open(DATABASE_FILE, 'w') as f:
+        json.dump(data, f, indent=4)
