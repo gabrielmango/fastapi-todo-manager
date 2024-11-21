983dbf31fc979cc86f8882b68933e0de68b6a873
Gabriel Mango <gabrielhmango@gmail.com>
2024-11-21 14:41:54 -0300

    feat: add get_todo_by_id() for Task CRUD

--- app/todo/crud.py
+++ app/todo/crud.py
@@ -3,3 +3,8 @@ from app.database import load_data, save_data
 
 def get_all_todos():
     return load_data()
+
+
+def get_todo_by_id(todo_id: int):
+    todos = load_data()
+    return next((todo for todo in todos if todo['id'] == todo_id), None)
