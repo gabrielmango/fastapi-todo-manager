39bf2a2b23cc7af221ba156e6c47e1154ff44cb2
Gabriel Mango <gabrielhmango@gmail.com>
2024-11-21 14:42:19 -0300

    feat: add create_todo() for Task CRUD

--- app/todo/crud.py
+++ app/todo/crud.py
@@ -8,3 +8,9 @@ def get_all_todos():
 def get_todo_by_id(todo_id: int):
     todos = load_data()
     return next((todo for todo in todos if todo['id'] == todo_id), None)
+
+
+def create_todo(todo: dict):
+    todos = load_data()
+    todos.append(todo)
+    save_data(todos)
