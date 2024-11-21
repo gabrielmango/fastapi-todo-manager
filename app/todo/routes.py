8226c7e8ffad921a8f01a2bdb07bd03d2585f13a
Gabriel Mango <gabrielhmango@gmail.com>
2024-11-21 14:58:37 -0300

    feat: create endpoint return one task

--- app/todo/routes.py
+++ app/todo/routes.py
@@ -1,6 +1,6 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, HTTPException
 
-from app.todo.crud import get_all_todos
+from app.todo.crud import get_all_todos, get_todo_by_id
 
 todo_router = APIRouter()
 
@@ -8,3 +8,11 @@ todo_router = APIRouter()
 @todo_router.get('/')
 def list_todos():
     return get_all_todos()
+
+
+@todo_router.get('/{todo_id}')
+def retrieve_todo(todo_id: int):
+    todo = get_todo_by_id(todo_id)
+    if not todo:
+        raise HTTPException(status_code=404, detail='Todo not found')
+    return todo
